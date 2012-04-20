15ece2b9c7b0e7657cbdf75ad64bcedd3892beba
aifgi <aifgi90@gmail.com>
2012-04-21 03:58:31 +0400

    JRE rus localization thanks: http://javatalks.ru/ftopic5784.php

---
+++ swing-view/src/com/sun/swing/internal/plaf/metal/resources/metal_ru.java
@@ -0,0 +1,79 @@
+package com.sun.swing.internal.plaf.metal.resources;
+
+import java.util.ListResourceBundle;
+
+public final class metal_ru extends ListResourceBundle {
+
+    public metal_ru() {
+    }
+
+    protected final Object[][] getContents() {
+        return (new Object[][]{
+                new Object[]{
+                        "FileChooser.detailsViewActionLabelText", "Таблица"
+                }, new Object[]{
+                "FileChooser.detailsViewButtonAccessibleName", "Таблица"
+        }, new Object[]{
+                "FileChooser.detailsViewButtonToolTipText", "Таблица"
+        }, new Object[]{
+                "FileChooser.fileAttrHeaderText", "Атрибуты"
+        }, new Object[]{
+                "FileChooser.fileDateHeaderText", "Modified"
+        }, new Object[]{
+                "FileChooser.fileNameHeaderText", "Имя"
+        }, new Object[]{
+                "FileChooser.fileNameLabelText", "Имя файла:"
+        }, new Object[]{
+                "FileChooser.fileSizeHeaderText", "Размер"
+        }, new Object[]{
+                "FileChooser.fileTypeHeaderText", "Тип"
+        }, new Object[]{
+                "FileChooser.filesOfTypeLabelText", "Тип файлов:"
+        }, new Object[]{
+                "FileChooser.homeFolderAccessibleName", "Домой"
+        }, new Object[]{
+                "FileChooser.homeFolderToolTipText", "Домой"
+        }, new Object[]{
+                "FileChooser.listViewActionLabelText", "Список"
+        }, new Object[]{
+                "FileChooser.listViewButtonAccessibleName", "Список"
+        }, new Object[]{
+                "FileChooser.listViewButtonToolTipText", "Список"
+        }, new Object[]{
+                "FileChooser.lookInLabelText", "Просмотр:"
+        }, new Object[]{
+                "FileChooser.newFolderAccessibleName", "Новая папка"
+        }, new Object[]{
+                "FileChooser.newFolderActionLabelText", "Новая папка"
+        }, new Object[]{
+                "FileChooser.newFolderToolTipText", "Создать новую папку"
+        }, new Object[]{
+                "FileChooser.refreshActionLabelText", "Обновить"
+        }, new Object[]{
+                "FileChooser.saveInLabelText", "Сохранить в:"
+        }, new Object[]{
+                "FileChooser.upFolderAccessibleName", "Вверх"
+        }, new Object[]{
+                "FileChooser.upFolderToolTipText", "На один уровень вверх"
+        }, new Object[]{
+                "FileChooser.viewMenuLabelText", "Просмотр"
+        }, new Object[]{
+                "MetalTitlePane.closeMnemonic", "67"
+        }, new Object[]{
+                "MetalTitlePane.closeTitle", "Закрыть"
+        }, new Object[]{
+                "MetalTitlePane.iconifyMnemonic", "69"
+        }, new Object[]{
+                "MetalTitlePane.iconifyTitle", "Свернуть"
+        }, new Object[]{
+                "MetalTitlePane.maximizeMnemonic", "88"
+        }, new Object[]{
+                "MetalTitlePane.maximizeTitle", "Развернуть"
+        }, new Object[]{
+                "MetalTitlePane.restoreMnemonic", "82"
+        }, new Object[]{
+                "MetalTitlePane.restoreTitle", "Восстановить"
+        }
+        });
+    }
+}
---
+++ swing-view/src/com/sun/swing/internal/plaf/synth/resources/synth_ru.java
@@ -0,0 +1,63 @@
+package com.sun.swing.internal.plaf.synth.resources;
+
+import java.util.ListResourceBundle;
+
+public final class synth_ru extends ListResourceBundle {
+
+    public synth_ru() {
+    }
+
+    protected final Object[][] getContents() {
+        return (new Object[][]{
+                new Object[]{
+                        "FileChooser.detailsViewActionLabelText", "Таблица"
+                }, new Object[]{
+                "FileChooser.detailsViewButtonAccessibleName", "Таблица"
+        }, new Object[]{
+                "FileChooser.detailsViewButtonToolTipText", "Таблица"
+        }, new Object[]{
+                "FileChooser.fileAttrHeaderText", "Атрибуты"
+        }, new Object[]{
+                "FileChooser.fileDateHeaderText", "Изменен"
+        }, new Object[]{
+                "FileChooser.fileNameHeaderText", "Имя"
+        }, new Object[]{
+                "FileChooser.fileNameLabelText", "Имя файла:"
+        }, new Object[]{
+                "FileChooser.fileSizeHeaderText", "Размер"
+        }, new Object[]{
+                "FileChooser.fileTypeHeaderText", "Тип"
+        }, new Object[]{
+                "FileChooser.filesOfTypeLabelText", "Тип файлов:"
+        }, new Object[]{
+                "FileChooser.homeFolderAccessibleName", "Домой"
+        }, new Object[]{
+                "FileChooser.homeFolderToolTipText", "Домой"
+        }, new Object[]{
+                "FileChooser.listViewActionLabelText", "Список"
+        }, new Object[]{
+                "FileChooser.listViewButtonAccessibleName", "Список"
+        }, new Object[]{
+                "FileChooser.listViewButtonToolTipText", "Список"
+        }, new Object[]{
+                "FileChooser.lookInLabelText", "Просмотр:"
+        }, new Object[]{
+                "FileChooser.newFolderAccessibleName", "Новая папка"
+        }, new Object[]{
+                "FileChooser.newFolderActionLabelText", "Новая папка"
+        }, new Object[]{
+                "FileChooser.newFolderToolTipText", "Создать новую папку"
+        }, new Object[]{
+                "FileChooser.refreshActionLabelText", "Обновить"
+        }, new Object[]{
+                "FileChooser.saveInLabelText", "Сохранить в:"
+        }, new Object[]{
+                "FileChooser.upFolderAccessibleName", "Вверх"
+        }, new Object[]{
+                "FileChooser.upFolderToolTipText", "Вверх на один уровень"
+        }, new Object[]{
+                "FileChooser.viewMenuLabelText", "Просмотр"
+        }
+        });
+    }
+}
